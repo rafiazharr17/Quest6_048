ea47934d7570b86f51921bf2cb0bc1022f2fadbf
rafiazharr17 <rafipilo17@gmail.com>
2024-11-21 23:28:20 +0700

    menambahkan data untuk opsi MataKuliah

--- app/src/main/java/com/rafi/pertemuan7/data/MataKuliah.kt
+++ app/src/main/java/com/rafi/pertemuan7/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.rafi.pertemuan7.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision"
+    )
 }
