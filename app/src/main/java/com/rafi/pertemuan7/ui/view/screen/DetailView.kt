deb8a8504cd648b66e2976aad7fdc6964087f2d7
rafiazharr17 <rafipilo17@gmail.com>
2024-11-22 01:05:55 +0700

    menambahkan fungsi untuk DetailMHS

--- app/src/main/java/com/rafi/pertemuan7/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/rafi/pertemuan7/ui/view/screen/DetailView.kt
@@ -1,6 +1,13 @@
 package com.rafi.pertemuan7.ui.view.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.rafi.pertemuan7.model.Mahasiswa
 import com.rafi.pertemuan7.model.RencanaStudi
 
@@ -19,4 +26,30 @@ fun DetailView(
         Pair("Mata Kuliah", dataKRS.mataKuliah),
         Pair("Kelas", dataKRS.kelas)
     )
+}
+
+@Composable
+fun DetailMHS(
+    judul: String,
+    isinya: String
+){
+    Column(
+        modifier = Modifier.fillMaxWidth().padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(top = 2.dp)
+        ) {
+            Text(
+                text = judul,
+                modifier = Modifier.weight(1f)
+            )
+            Text(text = ":",
+                modifier = Modifier.weight(0.2f)
+            )
+            Text(text = isinya,
+                modifier = Modifier.weight(2f)
+            )
+        }
+    }
 }
