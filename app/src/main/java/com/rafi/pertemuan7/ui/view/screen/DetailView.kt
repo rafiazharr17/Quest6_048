5b318e117b9d4ae5abad6dd66632ca392fe1f3af
rafiazharr17 <rafipilo17@gmail.com>
2024-11-22 01:00:01 +0700

    membuat fungsi untuk menampilkan detail data

---
+++ app/src/main/java/com/rafi/pertemuan7/ui/view/screen/DetailView.kt
@@ -0,0 +1,14 @@
+package com.rafi.pertemuan7.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import com.rafi.pertemuan7.model.Mahasiswa
+import com.rafi.pertemuan7.model.RencanaStudi
+
+@Composable
+fun DetailView(
+    dataMHS: Mahasiswa,
+    dataKRS: RencanaStudi,
+    onBackButtonClicked: () -> Unit
+){
+
+}
