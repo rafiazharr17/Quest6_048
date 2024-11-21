f33eb5b531ab3bc059b76045b238c5ca916d885d
rafiazharr17 <rafipilo17@gmail.com>
2024-11-22 00:00:44 +0700

    menambahkan fungsi saveDataKRS

--- app/src/main/java/com/rafi/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/rafi/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -18,4 +18,11 @@ class RencanaStudyViewModel : ViewModel() {
     fun setKelas(kelasPilihan: String){
         _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
     }
+
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            mataKuliah = ls[0],
+            kelas = ls[1]
+        ) }
+    }
 }
