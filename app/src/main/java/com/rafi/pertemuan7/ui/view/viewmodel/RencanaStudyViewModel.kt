5290759bfad2519a4c440114e0a01df9d792fac2
rafiazharr17 <rafipilo17@gmail.com>
2024-11-21 23:58:22 +0700

    menggunakan StateFlow untuk mengelola dan membagikan data RencanaStudi secara dinamis dengan akses read-only dari luar

--- app/src/main/java/com/rafi/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/rafi/pertemuan7/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,7 +1,15 @@
 package com.rafi.pertemuan7.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.rafi.pertemuan7.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
 
 }
