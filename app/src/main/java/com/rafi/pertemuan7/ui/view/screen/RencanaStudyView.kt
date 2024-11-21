2eca0bda5b8f53a3230fb125d4dcf18e3f18c630
rafiazharr17 <rafipilo17@gmail.com>
2024-11-22 00:42:19 +0700

    menambahkan checkbox untuk persetujuan

--- app/src/main/java/com/rafi/pertemuan7/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/rafi/pertemuan7/ui/view/screen/RencanaStudyView.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.RadioButton
@@ -161,6 +162,23 @@ fun RencanaStudyView(
                             Text(text = data)
                         }
                     }
+                }
+                Spacer(modifier = Modifier.padding(8.dp))
+                HorizontalDivider()
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(text = "Klausul Persetujuan Mahasiswa", fontWeight = FontWeight.Bold)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = {checked = it},
+                        enabled = chosenDropdown.isBlank() && pilihanKelas.isBlank()
+                    )
+                    Text(
+                        text = "Saya menyetujui setiap pernyataan yang ada tanpa ada paksaan dari pihak manapun.",
+                        fontWeight = FontWeight.Light, fontSize = 10.sp
+                    )
                 }
             }
         }
